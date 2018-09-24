3ab394d9cb36a2f8dbf393ed5c5c8fa2943dae88
Igor <sablin.igor@gmail.com>
2018-09-24 08:28:46 +0300

    Use environment variables

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,7 +12,7 @@ export function weatherGetData(city) {
   return (dispatch) => {
     dispatch(weatherIsLoading(true));
 
-    let url = 'http://api.apixu.com/v1/current.json?key=ef5d03b40b984087bc8135539182209&q=' + city;
+    let url = process.env.REACT_APP_WEATHER_API_URL + '?key=' + process.env.REACT_APP_WEATHER_API_KEY + '&q=' + city;
 
     axios.get(url)
       .then(res => {
